0e7a695b23eb68b3c18a0d77d04a9a2ab93dd5d5
SanteLGG <l.g.g.sante@gmail.com>
2025-08-26 22:52:03 -0300

    Initial commit

---
+++ src/test/java/io/github/santelgg/produtos_api/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.santelgg.produtos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
