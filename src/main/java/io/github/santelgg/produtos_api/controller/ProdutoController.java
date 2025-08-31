f3b76625db330078f38b833b9234a14523e55e06
luisguilherme <l.g.g.sante@gmail.com>
2025-08-31 16:08:33 -0300

    inclusão das classes produto e produ

--- src/main/java/io/github/santelgg/produtos_api/controller/ProdutoController.java
+++ src/main/java/io/github/santelgg/produtos_api/controller/ProdutoController.java
@@ -1,2 +1,16 @@
-package io.github.santelgg.produtos_api.controller;public class ProdutoController {
+package io.github.santelgg.produtos_api.controller;
+
+import io.github.santelgg.produtos_api.model.Produto;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PatchMapping
+    public void salvar(Produto produto){
+        System.out.println("Produto recebido: " + produto);
+    }
 }
--- src/main/java/io/github/santelgg/produtos_api/model/Produto.java
+++ src/main/java/io/github/santelgg/produtos_api/model/Produto.java
@@ -1,2 +1,56 @@
-package io.github.santelgg.produtos_api.model;public class Produto {
+package io.github.santelgg.produtos_api.model;
+
+    //POJO -> Plain Old Java Object
+    //Classe de negócios
+
+public class Produto {
+
+    private String id;
+    private String nome;
+    private String descrição;
+    private Double preco;
+
+    //Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrição() {
+        return descrição;
+    }
+
+    public void setDescrição(String descrição) {
+        this.descrição = descrição;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    //Representação em String do objeto
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descrição='" + descrição + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
 }
